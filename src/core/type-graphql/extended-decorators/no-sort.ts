a13c981cfd585b60e9b53ae8b176c9508ddc39c9
ezequielzacca <ezequielzacca@gmail.com>
2020-10-02 11:16:54 -0300

    @NoSort and @NoFilter decorators

---
+++ src/core/type-graphql/extended-decorators/no-sort.ts
@@ -0,0 +1,34 @@
+import { getMerlinMetadataStorage } from "../../../utils/metadata-storage";
+
+export const NoSort = () => {
+  return (prototype: any, propertyKey: any, descriptor: any) => {
+    const merlinGqlMetadataStorage = getMerlinMetadataStorage();
+    const keyName = Object.getPrototypeOf(prototype).constructor.name;
+    const existentMetadataForPrototype =
+      merlinGqlMetadataStorage.objectTypes[keyName];
+    if (existentMetadataForPrototype) {
+      existentMetadataForPrototype.fields.push({
+        name: propertyKey.toString(),
+        ignoreSort: false,
+        ignoreFilter: true,
+      });
+    } else {
+      merlinGqlMetadataStorage.objectTypes = {
+        ...merlinGqlMetadataStorage.objectTypes,
+        ...{
+          [keyName]: {
+            fields: [
+              {
+                name: propertyKey.toString(),
+                ignoreSort: false,
+                ignoreFilter: true,
+              },
+            ],
+            extends: null,
+          },
+        },
+      };
+    }
+    //Wrap typegraphql field decorator
+  };
+};

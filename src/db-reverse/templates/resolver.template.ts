3d9bc0fdbd001edbda76f6dc4aaa96193d4ed313
ezequielzacca <ezequielzacca@gmail.com>
2020-10-05 13:20:50 -0300

    resolver template based on metadata

--- src/db-reverse/templates/resolver.template.ts
+++ src/db-reverse/templates/resolver.template.ts
@@ -1,3 +1,4 @@
+import { resolverIncludesOperation } from "./../../utils/metadata-storage";
 import {
   toEntityFileName,
   toEntityName,
@@ -19,6 +20,53 @@ export const ResolverTemplate = (
     const fileName:string = toFileName(tscName, generationOptions)
     const filtersName:string = toFiltersName(tscName, generationOptions);
     const sortsName:string = toSortsName(tscName, generationOptions);
+
+    //list resolver based on metadata
+    const shouldGenerateListResolver = resolverIncludesOperation(entityName,"LIST")
+    const listResolver:string = shouldGenerateListResolver ? `
+    const BaseListResolver = ListResolver(${entityName});
+    @Resolver()
+    ${secureResolverDecorator}
+    export class ${entityName}ListResolver extends BaseListResolver<${entityName}, ${filtersName}, ${sortsName}> {}
+    ` : ""
+
+    //find resolver based on metadata
+    const shouldGenerateFindResolver = resolverIncludesOperation(entityName,"FIND")
+    const findResolver:string = shouldGenerateFindResolver ? `
+    const BaseFindResolver = FindResolver(${entityName});
+    @Resolver()
+    ${secureResolverDecorator}
+    export class ${entityName}FindResolver extends BaseFindResolver<${entityName}> {}
+    ` : ""
+
+    //update resolver based on metadata
+    const shouldGenerateUpdateResolver = resolverIncludesOperation(entityName,"UPDATE")
+    const updateResolver:string = shouldGenerateUpdateResolver ? `
+    const BaseUpdateResolver = UpdateResolver(${entityName});
+    @Resolver()
+    ${secureResolverDecorator}
+    export class ${entityName}UpdateResolver extends BaseUpdateResolver<${entityName}> {}
+    ` : ""   
+
+    //create resolver based on metadata
+    const shouldGenerateCreateResolver = resolverIncludesOperation(entityName,"CREATE")
+    const createResolver:string = shouldGenerateCreateResolver ? `
+    const BaseCreateResolver = CreateResolver(${entityName});
+    @Resolver()
+    ${secureResolverDecorator}
+    export class ${entityName}CreateResolver extends BaseCreateResolver<${entityName}> {}
+    ` : ""   
+
+    //create resolver based on metadata
+    const shouldGenerateDeleteResolver = resolverIncludesOperation(entityName,"DELETE")
+    const deleteResolver:string = shouldGenerateDeleteResolver ? `
+    const BaseDeleteResolver = DeleteResolver(${entityName});
+    @Resolver()
+    ${secureResolverDecorator}
+    export class ${entityName}DeleteResolver extends BaseDeleteResolver<${entityName}> {}
+    ` : ""   
+
+
     return `
     
     import { Resolver } from "type-graphql";
@@ -27,31 +75,21 @@ export const ResolverTemplate = (
     import ${toLocalImport(sortsName, generationOptions)} from "../models/${fileName}/${fileName}.sort";
     import { CreateResolver, DeleteResolver, FindResolver, ListResolver, ${secureImport} UpdateResolver } from "merlin-gql";
     
-    const BaseListResolver = ListResolver(${entityName});
-    const BaseFindResolver = FindResolver(${entityName});
-    const BaseCreateResolver = CreateResolver(${entityName});
-    const BaseUpdateResolver = UpdateResolver(${entityName});
-    const BaseDeleteResolver = DeleteResolver(${entityName});
     
-    @Resolver()
-    ${secureResolverDecorator}
-    export class ${entityName}ListResolver extends BaseListResolver<${entityName}, ${filtersName}, ${sortsName}> {}
     
-    @Resolver()
-    ${secureResolverDecorator}
-    export class ${entityName}FindResolver extends BaseFindResolver<${entityName}> {}
     
-    @Resolver()
-    ${secureResolverDecorator}
-    export class ${entityName}UpdateResolver extends BaseUpdateResolver<${entityName}> {}
     
-    @Resolver()
-    ${secureResolverDecorator}
-    export class ${entityName}CreateResolver extends BaseCreateResolver<${entityName}> {}
     
-    @Resolver()
-    ${secureResolverDecorator}
-    export class ${entityName}DeleteResolver extends BaseDeleteResolver<${entityName}> {}
+    
+    ${listResolver}    
+    
+    ${findResolver}    
+    
+    ${updateResolver}    
+    
+    ${createResolver}    
+    
+    ${deleteResolver}    
     
     `
 }

643a33f4d09964bb3e476325f47d7461a863a900
Ezequiel Zacca <ezequielzacca@MacBook-Air-de-Ezequiel.local>
2021-01-24 09:06:43 -0300

    ISSUE-63 fixing nosort and nofilter decorators

--- src/core/type-graphql/extended-decorators/no-sort.ts
+++ src/core/type-graphql/extended-decorators/no-sort.ts
@@ -1,7 +1,7 @@
 import { addNoSortMetadata } from "./../../../utils/metadata-storage";
 
-export const NoSort = (): MethodDecorator | PropertyDecorator => {
-  return (prototype, propertyKey, descriptor) => {
+export const NoSort = (): PropertyDecorator => {
+  return (prototype, propertyKey): void => {
     const keyName = Object.getPrototypeOf(prototype).constructor.name;
     addNoSortMetadata(keyName, propertyKey.toString());
     //Wrap typegraphql field decorator
